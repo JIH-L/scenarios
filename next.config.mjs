2313b676fc55cea4e0f758ea9d08c37e92e1560e
JIH L <sol@livemail.tw>
2024-01-23 14:04:13 +0800

    chore: add aws-sdk & mime-type

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
   },
+  images: {
+    domains: ['scenarios-bucket.s3.amazonaws.com'],
+  },
 };
 
 export default nextConfig;
