cc23861e6fb870c4fa57ce6a377f842aae1d9e31
JIH L <sol@livemail.tw>
2024-01-23 14:05:09 +0800

    feat: update gmaes POST API

--- src/pages/api/games.js
+++ src/pages/api/games.js
@@ -1,8 +1,9 @@
-// pages/api/upload.js
 import { connectToDatabase } from "./connect-mongo";
 import formidable from "formidable";
 import fs from "fs";
 import path from "path";
+import AWS from "aws-sdk";
+import mime from "mime-types";
 
 export const config = {
   api: {
@@ -16,17 +17,16 @@ function getFieldValue(field) {
 
 export default async function handler(req, res) {
   const db = await connectToDatabase(process.env.MONGODB_URI);
+  AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+  });
+
+  const s3 = new AWS.S3();
 
   if (req.method === "POST") {
     const form = formidable({});
-    form.uploadDir = "./public/images"; // 指定文件上傳目錄
-    form.keepExtensions = true; // 保留文件擴展名
-    form.on("fileBegin", (name, file) => {
-      const ext = path.extname(file.originalFilename);
-      // 使用 formidable 生成的 newFilename 作为文件名
-      file.filepath = path.join(form.uploadDir, file.newFilename + ext);
-    });
-
     form.parse(req, async (err, fields, files) => {
       try {
         if (err) {
@@ -34,27 +34,47 @@ export default async function handler(req, res) {
           return;
         }
 
-        // 從 files 對象獲取文件信息
+        // 從 files 取得文件資訊
         const uploadedFile = files.image;
+        const fileContent = fs.readFileSync(uploadedFile[0].filepath);
+
         // 取得副檔名
         const ext = path.extname(uploadedFile[0].originalFilename);
-        // 構造出可訪問的 URL
-        const imageUrl = `/images/${uploadedFile[0].newFilename + ext}`;
+
+        // 文件在 S3 的儲存路徑
+        const s3Key = `images/${uploadedFile[0].newFilename + ext}`;
+
+        // 取得文件的 MIME Type
+        const contentType = mime.lookup(uploadedFile[0].originalFilename);
+
+        // 上傳到 S3
+        const params = {
+          Bucket: "scenarios-bucket",
+          Key: s3Key,
+          Body: fileContent,
+          ContentType: contentType,
+          ACL: "public-read",
+        };
+        const s3Upload = await s3.upload(params).promise();
+
+        // 取得 S3 URL
+        const imageUrl = s3Upload.Location;
+
+        // 準備要儲存到 MongoDB 的資料
         const title = getFieldValue(fields.title);
         const content = getFieldValue(fields.content);
         const data = {
           title,
           content,
-          imageUrl, // 使用 imageUrl 作為圖片的 URL
+          imageUrl, // 使用 S3 圖片的 URL
         };
 
-        // 根據需要將數據保存到數據庫，進行其他處理
+        // 儲存到 MongoDB
         const result = await db.collection("games").insertOne(data);
 
         res.status(200).json({ message: "文件上傳成功", file: data });
       } catch (error) {
-        // 如果有任何錯誤，發送一個 500 錯誤響應
-        res.status(500).json({ message: "服務器錯誤", error: error.message });
+        res.status(500).json({ message: "Server Error", error: error.message });
       }
     });
   } else if (req.method === "GET") {
