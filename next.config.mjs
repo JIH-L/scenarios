c54ca07b4b6a6302ad84393a75d388735aca18e1
JIH L <sol@livemail.tw>
2024-01-23 15:00:10 +0800

    feat: update next config images domain to remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,12 @@ const nextConfig = {
     NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
   },
   images: {
-    domains: ['scenarios-bucket.s3.amazonaws.com', 'scenarios-bucket.s3.us-east-1.amazonaws.com'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "scenarios-bucket.s3.us-east-1.amazonaws.com",
+      },
+    ],
   },
 };
 
