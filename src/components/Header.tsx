ea00cbeb5a291f4ff0828594832ee1a4c6bdda31
JIH L <sol@livemail.tw>
2024-01-30 16:27:50 +0800

    feat: update header sticky

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,10 @@ import Link from "next/link"
 import { Button } from "@/components/ui/button"
 export default function Header() {
     return (
-        <header className="flex items-center justify-between py-4 px-8 border-b-[1px]">
+        <header className="flex items-center justify-between py-2 px-8 border-b-[1px] sticky top-0 backdrop-blur bg-background/95 supports-[backdrop-filter]:bg-background/60">
             <div className="flex items-center justify-start space-x-8">
-                <Link href="/" className="text-3xl font-bold flex items-center gap-2">
-                    <Image src="/images/logo.svg" alt="Scenarios" width={40} height={40} />
+                <Link href="/" className="text-xl font-bold flex items-center gap-1">
+                    <Image src="/images/logo.svg" alt="Scenarios" width={30} height={30} />
                     ChatScripter
                 </Link>
                 <nav className="hidden md:flex">
