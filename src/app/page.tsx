a7ff9da1a5298c1df309f77f34a947635369f4f9
JIH L <sol@livemail.tw>
2024-02-02 16:25:26 +0800

    feat: add h1 on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,28 +37,37 @@ export default function Home() {
 
   return (
     <>
-      <section className="my-10">
-      <h2>Games</h2>
-      {loading && (
-        <div className="grid grid-cols-3 gap-8">
-          {Array(6)
-            .fill(null)
-            .map((_, index) => (
-              <SkeletonCard key={index} />
-            ))}
+      <section className="flex flex-col space-x-2 gap-2 items-center py-20">
+        <h1 className="text-6xl text-center leading-[1.1]">
+          AI Powered Script Writing
+        </h1>
+        <span className="text-center max-w-xl text-gray-500">
+          Explore ChatScripter, an innovative platform where AI technology meets
+          creative scriptwriting. Generate unique scripts effortlessly with AI.
+        </span>
+      </section>
+      <section className="py-10">
+        <h2>Games</h2>
+        {loading && (
+          <div className="grid grid-cols-3 gap-8">
+            {Array(6)
+              .fill(null)
+              .map((_, index) => (
+                <SkeletonCard key={index} />
+              ))}
+          </div>
+        )}
+        <div
+          className={`grid grid-cols-3 gap-8 transition-opacity duration-500 ${
+            loading ? "opacity-0" : "opacity-100"
+          }`}
+        >
+          {gamesList.map((game, index) => (
+            <Link href={`/games/${game._id}`} key={game._id}>
+              <ArticleCard data={game} priority={index < 3} />
+            </Link>
+          ))}
         </div>
-      )}
-      <div
-        className={`grid grid-cols-3 gap-8 transition-opacity duration-500 ${
-          loading ? "opacity-0" : "opacity-100"
-        }`}
-      >
-        {gamesList.map((game, index) => (
-          <Link href={`/games/${game._id}`} key={game._id}>
-            <ArticleCard data={game}  priority={index < 3}/>
-          </Link>
-        ))}
-      </div>
       </section>
     </>
   );
