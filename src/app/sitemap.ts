8746202acab0da6474a142ff93130c09aec3611d
JIH-L <sol@livemail.tw>
2024-07-01 23:02:16 +0800

    feat: update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,28 @@
 import { MetadataRoute } from 'next';
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const games = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/games`).then(
+    (res) => res.json()
+  );
+  const gamesSitemap =
+    games?.data.map((game: any) => ({
+      url: `${process.env.NEXT_PUBLIC_URL}/game/${game._id}`,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.5,
+    })) || [];
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
+  const novels = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/novels`).then(
+    (res) => res.json()
+  );
+  const novelsSitemap =
+    novels?.data.map((novel: any) => ({
+      url: `${process.env.NEXT_PUBLIC_URL}/novel/${novel._id}`,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.5,
+    })) || [];
+
+  const sitemap = [
     {
       url: `${process.env.NEXT_PUBLIC_URL}`,
       lastModified: new Date(),
@@ -20,5 +41,9 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.8,
     },
+    ...gamesSitemap,
+    ...novelsSitemap,
   ];
+
+  return sitemap;
 }
